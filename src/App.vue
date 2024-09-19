c9532f2c229d17baf3c65b5d3ac4a001506348ce
Mary <mary.pommer@tptlive.ee>
2024-09-19 15:56:53 +0300

    add font-family

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <v-app>
+    <h1 hidden>This is a Frontend Mentor testimonial grid layout challenge</h1>
     <v-main>
       <router-view />
     </v-main>
+
   </v-app>
 </template>
 
